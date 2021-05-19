f05fef0f4b93e636ab7c4aa1d38f6c20587ddeb3
Cristi Cretu <cristicretu38@gmail.com>
2021-05-19 19:44:56 +0300

    I made Google basically

--- pset9/hello/app.py
+++ pset9/hello/app.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 
 @app.route("/index.html")
 def index():
-    return render_template("index.html", name=request.args.get("name", "world"))
+    return render_template("index.html")
+
+@app.route("/greet")
+def greet():
+    return render_template("greet.html", name=request.args.get("name", "world"))
