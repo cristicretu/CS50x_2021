9783f4ab612d611c223a1ac5d0da7b951c237759
Cristi Cretu <cristicretu38@gmail.com>
2021-05-19 19:04:33 +0300

    Safe Version

--- pset9/hello/app.py
+++ pset9/hello/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # Flask turn the current file into an application
 app = Flask(__name__)
 
 @app.route("/index.html")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", name=request.args.get("name", "world"))
