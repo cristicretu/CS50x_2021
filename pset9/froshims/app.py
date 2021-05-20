81dd7cdba5e10b051f43ec4da654b18620d4918b
Cristi Cretu <cristicretu38@gmail.com>
2021-05-20 10:03:59 +0300

    Add froshims

---
+++ pset9/froshims/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+REGISTRANT = {}
+
+SPORTS = [
+    "Dodgeball",
+    "Flag Football",
+    "Soccer",
+    "Volleyball",
+    "Ultimate Frisbee"
+]
+
+@app.route("/")
+def index():
+    return render_template("index.html", sports=SPORTS)
+
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.form.get("name")
+    if not name:
+        return render_template("error.html", message="Missing name")
+    sport = request.form.get("sport")
+    if not sport:
+        return render_template("error.html", message="Missing sport")
+    if sport not in SPORTS:
+        return render_template("error.html", message="Invalid sport")
+    
+    REGISTRANT[name] = sport
+    return render_template("succes.html")
+    
