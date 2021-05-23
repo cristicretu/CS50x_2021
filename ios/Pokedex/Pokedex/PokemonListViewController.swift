2afe4149fea7d64b471cb431855ebea97e67c8a0
Cristi Cretu <cristicretu38@gmail.com>
2021-05-23 22:14:10 +0300

    Add unfinished Pokedex

---
+++ ios/Pokedex/Pokedex/PokemonListViewController.swift
@@ -0,0 +1,80 @@
+import UIKit
+
+// Search inspired from here:
+// https://stackoverflow.com/questions/41663233/tableview-search-in-swift
+
+class PokemonListViewController: UITableViewController, UISearchBarDelegate {
+    @IBOutlet var searchBar: UISearchBar!
+    
+    var pokemon: [PokemonListResult] = []
+    var filteredPokemon: [PokemonListResult]!
+    
+    func capitalize(text: String) -> String {
+        return text.prefix(1).uppercased() + text.dropFirst()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        searchBar.delegate = self
+        filteredPokemon = pokemon
+        
+        guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon?limit=151") else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                let entries = try JSONDecoder().decode(PokemonListResults.self, from: data)
+                self.pokemon = entries.results
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+            catch let error {
+                print(error)
+            }
+        }.resume()
+    }
+    
+    // This method updates filteredData based on the text in the Search Box
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // When there is no text, filteredData is the same as the original data
+        // When user has entered text into the search box
+        // Use the filter method to iterate over all items in the data array
+        // For each item, return true if the item should be included and false if the
+        // item should NOT be included
+        filteredPokemon = searchText.isEmpty ? pokemon : pokemon.filter({(pokemonString: PokemonListResult) -> Bool in
+            // If dataItem matches the searchText, return true to include it
+            return pokemonString.range(of: searchText, options: .caseInsensitive) != nil
+        })
+
+        tableView.reloadData()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredPokemon.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PokemonCell", for: indexPath)
+        cell.textLabel?.text = capitalize(text: filteredPokemon[indexPath.row].name)
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowPokemonSegue",
+                let destination = segue.destination as? PokemonViewController,
+                let index = tableView.indexPathForSelectedRow?.row {
+            destination.url = pokemon[index].url
+        }
+    }
+}
