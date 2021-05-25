0dceaaf34450e9cc1a435632339e4a907b807b71
Cristi Cretu <cristicretu38@gmail.com>
2021-05-25 13:26:44 +0300

    WTF IS THIS

---
+++ ios/Fiftygram/Fiftygram/ViewController.swift
@@ -0,0 +1,77 @@
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet var imageView: UIImageView!
+
+    let context = CIContext()
+    var original: UIImage!
+
+    @IBAction func applySepia() {
+        if original == nil {
+            return
+        }
+
+        let filter = CIFilter(name: "CISepiaTone")
+        filter?.setValue(CIImage(image: original), forKey: kCIInputImageKey)
+        filter?.setValue(1.0, forKey: kCIInputIntensityKey)
+        display(filter: filter!)
+    }
+    
+    
+    @IBAction func applyInvert() {
+        if original == nil {
+            return
+        }
+
+        let filter = CIFilter(name: "CIGaussianBlur")
+        filter?.setValue(original, forKey: kCIInputImageKey)
+        filter?.setValue(10.0, forKey: kCIInputRadiusKey)
+//                let outputImage = filter?.outputImage
+        display(filter: filter!)
+    }
+    
+    @IBAction func applyNoir() {
+        if original == nil {
+            return
+        }
+
+        let filter = CIFilter(name: "CIPhotoEffectNoir")
+        filter?.setValue(CIImage(image: original), forKey: kCIInputImageKey)
+        display(filter: filter!)
+    }
+
+    @IBAction func applyVintage() {
+        if original == nil {
+            return
+        }
+
+        let filter = CIFilter(name: "CIPhotoEffectProcess")
+        filter?.setValue(CIImage(image: original), forKey: kCIInputImageKey)
+        display(filter: filter!)
+    }
+
+    @IBAction func choosePhoto(_ sender: UIBarButtonItem) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = .photoLibrary
+            self.navigationController?.present(picker, animated: true, completion: nil)
+        }
+    }
+
+    func display(filter: CIFilter) {
+        let output = filter.outputImage!
+        imageView.image = UIImage(cgImage: self.context.createCGImage(output, from: output.extent)!)
+    }
+
+    func imagePickerController(
+        _ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]
+    ) {
+        self.navigationController?.dismiss(animated: true, completion: nil)
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = image
+            original = image
+        }
+    }
+}
